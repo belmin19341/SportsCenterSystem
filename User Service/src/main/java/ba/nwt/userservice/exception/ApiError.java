ec38667430f72e2db5b2fb8b24024490e1511f9e
belmin19341 <bdurmo1@etf.unsa.ba>
2026-04-02 19:05:51 +0200

    Add ApiError class and AppConfig for model mapping across services

---
+++ User Service/src/main/java/ba/nwt/userservice/exception/ApiError.java
@@ -0,0 +1,15 @@
+package ba.nwt.userservice.exception;
+
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ApiError {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private List<String> details;
+}
+
